9f158f64c7b984e56df41f098760cee162ca43e8
Vazgen <76519446+vazgenM2@users.noreply.github.com>
2021-11-04 21:57:04 +0400

    Add files via upload

--- code.js
+++ code.js
@@ -56,119 +56,119 @@ function setNumbers(res) {
 
 	// ================================ IRISH LUCKY
 	for (let i = 0; i < document.querySelector('.irish-part-1 .irish-main .general-part').childElementCount; i++) {
-		document.querySelector('.irish-part-1 .irish-main .general-part').children[i].innerHTML = res.irish_lucky[0].numbers[0].main[i]
-		document.querySelector('.irish-part-1 .irish-plus-1 .general-part').children[i].innerHTML = res.irish_lucky[0].numbers[1].plus_1[i]
-		document.querySelector('.irish-part-1 .irish-plus-2 .general-part').children[i].innerHTML = res.irish_lucky[0].numbers[2].plus_2[i]
+		document.querySelector('.irish-part-1 .irish-main .general-part').children[i].innerHTML = res.irish_lucky[0].numbers.main[i]
+		document.querySelector('.irish-part-1 .irish-plus-1 .general-part').children[i].innerHTML = res.irish_lucky[0].numbers.plus_1[i]
+		document.querySelector('.irish-part-1 .irish-plus-2 .general-part').children[i].innerHTML = res.irish_lucky[0].numbers.plus_2[i]
 
-		document.querySelector('.irish-part-2 .irish-main .general-part').children[i].innerHTML = res.irish_lucky[1].numbers[0].main[i]
-		document.querySelector('.irish-part-2 .irish-plus-1 .general-part').children[i].innerHTML = res.irish_lucky[1].numbers[1].plus_1[i]
-		document.querySelector('.irish-part-2 .irish-plus-2 .general-part').children[i].innerHTML = res.irish_lucky[1].numbers[2].plus_2[i]
+		document.querySelector('.irish-part-2 .irish-main .general-part').children[i].innerHTML = res.irish_lucky[1].numbers.main[i]
+		document.querySelector('.irish-part-2 .irish-plus-1 .general-part').children[i].innerHTML = res.irish_lucky[1].numbers.plus_1[i]
+		document.querySelector('.irish-part-2 .irish-plus-2 .general-part').children[i].innerHTML = res.irish_lucky[1].numbers.plus_2[i]
 
-		document.querySelector('.irish-part-3 .irish-main .general-part').children[i].innerHTML = res.irish_lucky[2].numbers[0].main[i]
-		document.querySelector('.irish-part-3 .irish-plus-1 .general-part').children[i].innerHTML = res.irish_lucky[2].numbers[1].plus_1[i]
-		document.querySelector('.irish-part-3 .irish-plus-2 .general-part').children[i].innerHTML = res.irish_lucky[2].numbers[2].plus_2[i]
+		document.querySelector('.irish-part-3 .irish-main .general-part').children[i].innerHTML = res.irish_lucky[2].numbers.main[i]
+		document.querySelector('.irish-part-3 .irish-plus-1 .general-part').children[i].innerHTML = res.irish_lucky[2].numbers.plus_1[i]
+		document.querySelector('.irish-part-3 .irish-plus-2 .general-part').children[i].innerHTML = res.irish_lucky[2].numbers.plus_2[i]
 
 	}
 	// ==== IRISH BOUNUS BALLS 
-	document.querySelector('.irish-part-1 .irish-main .bonus-part  span').innerHTML = res.irish_lucky[0].numbers[0].bonusball
-	document.querySelector('.irish-part-1 .irish-plus-1 .bonus-part span').innerHTML = res.irish_lucky[0].numbers[1].bonusball
-	document.querySelector('.irish-part-1 .irish-plus-2 .bonus-part span').innerHTML = res.irish_lucky[0].numbers[2].bonusball
+	document.querySelector('.irish-part-1 .irish-main .bonus-part  span').innerHTML = res.irish_lucky[0].numbers.bonusball_main
+	document.querySelector('.irish-part-1 .irish-plus-1 .bonus-part span').innerHTML = res.irish_lucky[0].numbers.bonusball_plus_1
+	document.querySelector('.irish-part-1 .irish-plus-2 .bonus-part span').innerHTML = res.irish_lucky[0].numbers.bonusball_plus_2
 
-	document.querySelector('.irish-part-2 .irish-main .bonus-part span').innerHTML = res.irish_lucky[1].numbers[0].bonusball
-	document.querySelector('.irish-part-2 .irish-plus-1 .bonus-part span').innerHTML = res.irish_lucky[1].numbers[1].bonusball
-	document.querySelector('.irish-part-2 .irish-plus-2 .bonus-part span').innerHTML = res.irish_lucky[1].numbers[2].bonusball
+	document.querySelector('.irish-part-2 .irish-main .bonus-part span').innerHTML = res.irish_lucky[1].numbers.bonusball_main
+	document.querySelector('.irish-part-2 .irish-plus-1 .bonus-part span').innerHTML = res.irish_lucky[1].numbers.bonusball_plus_1
+	document.querySelector('.irish-part-2 .irish-plus-2 .bonus-part span').innerHTML = res.irish_lucky[1].numbers.bonusball_plus_2
 
-	document.querySelector('.irish-part-3 .irish-main .bonus-part span').innerHTML = res.irish_lucky[2].numbers[0].bonusball
-	document.querySelector('.irish-part-3 .irish-plus-1 .bonus-part span').innerHTML = res.irish_lucky[2].numbers[1].bonusball
-	document.querySelector('.irish-part-3 .irish-plus-2 .bonus-part span').innerHTML = res.irish_lucky[2].numbers[2].bonusball
+	document.querySelector('.irish-part-3 .irish-main .bonus-part span').innerHTML = res.irish_lucky[2].numbers.bonusball_main
+	document.querySelector('.irish-part-3 .irish-plus-1 .bonus-part span').innerHTML = res.irish_lucky[2].numbers.bonusball_plus_1
+	document.querySelector('.irish-part-3 .irish-plus-2 .bonus-part span').innerHTML = res.irish_lucky[2].numbers.bonusball_plus_2
 
 	// ================================ EURO LUCKY
 	for (let i = 0; i < document.querySelector('.euro-part-1 .euro-main .general-part').childElementCount; i++) {
-		document.querySelector('.euro-part-1 .euro-main .general-part').children[i].innerHTML = res.euro_lucky[0].numbers[0].main[i]
-		document.querySelector('.euro-part-1 .euro-plus .general-part').children[i].innerHTML = res.euro_lucky[0].numbers[1].plus[i]
+		document.querySelector('.euro-part-1 .euro-main .general-part').children[i].innerHTML = res.euro_lucky[0].numbers.main[i]
+		document.querySelector('.euro-part-1 .euro-plus .general-part').children[i].innerHTML = res.euro_lucky[0].numbers.plus[i]
 
-		document.querySelector('.euro-part-2 .euro-main .general-part').children[i].innerHTML = res.euro_lucky[1].numbers[0].main[i]
-		document.querySelector('.euro-part-2 .euro-plus .general-part').children[i].innerHTML = res.euro_lucky[1].numbers[1].plus[i]
+		document.querySelector('.euro-part-2 .euro-main .general-part').children[i].innerHTML = res.euro_lucky[1].numbers.main[i]
+		document.querySelector('.euro-part-2 .euro-plus .general-part').children[i].innerHTML = res.euro_lucky[1].numbers.plus[i]
 
-		document.querySelector('.euro-part-3 .euro-main .general-part').children[i].innerHTML = res.euro_lucky[2].numbers[0].main[i]
-		document.querySelector('.euro-part-3 .euro-plus .general-part').children[i].innerHTML = res.euro_lucky[2].numbers[1].plus[i]
+		document.querySelector('.euro-part-3 .euro-main .general-part').children[i].innerHTML = res.euro_lucky[2].numbers.main[i]
+		document.querySelector('.euro-part-3 .euro-plus .general-part').children[i].innerHTML = res.euro_lucky[2].numbers.plus[i]
 
-		document.querySelector('.euro-part-4 .euro-main .general-part').children[i].innerHTML = res.euro_lucky[3].numbers[0].main[i]
-		document.querySelector('.euro-part-4 .euro-plus .general-part').children[i].innerHTML = res.euro_lucky[3].numbers[1].plus[i]
+		document.querySelector('.euro-part-4 .euro-main .general-part').children[i].innerHTML = res.euro_lucky[3].numbers.main[i]
+		document.querySelector('.euro-part-4 .euro-plus .general-part').children[i].innerHTML = res.euro_lucky[3].numbers.plus[i]
 	}
 
 	// ==== EURO BOUNUS BALLS
-	document.querySelectorAll('.euro-part-1 .euro-main .bonus-part  span')[0].innerHTML = res.euro_lucky[0].numbers[2].lucky_stars[0]
-	document.querySelectorAll('.euro-part-1 .euro-main .bonus-part  span')[1].innerHTML = res.euro_lucky[0].numbers[2].lucky_stars[1]
-	document.querySelectorAll('.euro-part-2 .euro-main .bonus-part span')[0].innerHTML = res.euro_lucky[1].numbers[2].lucky_stars[0]
-	document.querySelectorAll('.euro-part-2 .euro-main .bonus-part span')[1].innerHTML = res.euro_lucky[1].numbers[2].lucky_stars[1]
-	document.querySelectorAll('.euro-part-3 .euro-main .bonus-part span')[0].innerHTML = res.euro_lucky[2].numbers[2].lucky_stars[0]
-	document.querySelectorAll('.euro-part-3 .euro-main .bonus-part span')[1].innerHTML = res.euro_lucky[2].numbers[2].lucky_stars[1]
-	document.querySelectorAll('.euro-part-4 .euro-main .bonus-part span')[0].innerHTML = res.euro_lucky[3].numbers[2].lucky_stars[0]
-	document.querySelectorAll('.euro-part-4 .euro-main .bonus-part span')[1].innerHTML = res.euro_lucky[3].numbers[2].lucky_stars[1]
+	document.querySelectorAll('.euro-part-1 .euro-main .bonus-part  span')[0].innerHTML = res.euro_lucky[0].numbers.stars[0]
+	document.querySelectorAll('.euro-part-1 .euro-main .bonus-part  span')[1].innerHTML = res.euro_lucky[0].numbers.stars[1]
+	document.querySelectorAll('.euro-part-2 .euro-main .bonus-part span')[0].innerHTML = res.euro_lucky[1].numbers.stars[0]
+	document.querySelectorAll('.euro-part-2 .euro-main .bonus-part span')[1].innerHTML = res.euro_lucky[1].numbers.stars[1]
+	document.querySelectorAll('.euro-part-3 .euro-main .bonus-part span')[0].innerHTML = res.euro_lucky[2].numbers.stars[0]
+	document.querySelectorAll('.euro-part-3 .euro-main .bonus-part span')[1].innerHTML = res.euro_lucky[2].numbers.stars[1]
+	document.querySelectorAll('.euro-part-4 .euro-main .bonus-part span')[0].innerHTML = res.euro_lucky[3].numbers.stars[0]
+	document.querySelectorAll('.euro-part-4 .euro-main .bonus-part span')[1].innerHTML = res.euro_lucky[3].numbers.stars[1]
 
 
 	// ================================ 49's LUCKY
 	for (let i = 0; i < document.querySelector('.s49-part-1 .s49-lunchtime .general-part').childElementCount; i++) {
-		document.querySelector('.s49-part-1 .s49-lunchtime .general-part').children[i].innerHTML = res.forty_nine[0].numbers[0].lunchtime[i]
-		document.querySelector('.s49-part-1 .s49-teatime .general-part').children[i].innerHTML = res.forty_nine[0].numbers[1].teatime[i]
+		document.querySelector('.s49-part-1 .s49-lunchtime .general-part').children[i].innerHTML = res.forty_nine[0].numbers.lunchtime[i]
+		document.querySelector('.s49-part-1 .s49-teatime .general-part').children[i].innerHTML = res.forty_nine[0].numbers.teatime[i]
 
-		document.querySelector('.s49-part-2 .s49-lunchtime .general-part').children[i].innerHTML = res.forty_nine[1].numbers[0].lunchtime[i]
-		document.querySelector('.s49-part-2 .s49-teatime .general-part').children[i].innerHTML = res.forty_nine[1].numbers[1].teatime[i]
+		document.querySelector('.s49-part-2 .s49-lunchtime .general-part').children[i].innerHTML = res.forty_nine[1].numbers.lunchtime[i]
+		document.querySelector('.s49-part-2 .s49-teatime .general-part').children[i].innerHTML = res.forty_nine[1].numbers.teatime[i]
 
-		document.querySelector('.s49-part-3 .s49-lunchtime .general-part').children[i].innerHTML = res.forty_nine[2].numbers[0].lunchtime[i]
-		document.querySelector('.s49-part-3 .s49-teatime .general-part').children[i].innerHTML = res.forty_nine[2].numbers[1].teatime[i]
+		document.querySelector('.s49-part-3 .s49-lunchtime .general-part').children[i].innerHTML = res.forty_nine[2].numbers.lunchtime[i]
+		document.querySelector('.s49-part-3 .s49-teatime .general-part').children[i].innerHTML = res.forty_nine[2].numbers.teatime[i]
 
-		document.querySelector('.s49-part-4 .s49-lunchtime .general-part').children[i].innerHTML = res.forty_nine[3].numbers[0].lunchtime[i]
-		document.querySelector('.s49-part-4 .s49-teatime .general-part').children[i].innerHTML = res.forty_nine[3].numbers[1].teatime[i]
+		document.querySelector('.s49-part-4 .s49-lunchtime .general-part').children[i].innerHTML = res.forty_nine[3].numbers.lunchtime[i]
+		document.querySelector('.s49-part-4 .s49-teatime .general-part').children[i].innerHTML = res.forty_nine[3].numbers.teatime[i]
 	}
 	// ========== 49's BOUNUS BALLS
-	document.querySelector('.s49-part-1 .s49-lunchtime .bonus-part  span').innerHTML = res.forty_nine[0].numbers[0].buster
-	document.querySelector('.s49-part-1 .s49-teatime .bonus-part span').innerHTML = res.forty_nine[0].numbers[1].buster
+	document.querySelector('.s49-part-1 .s49-lunchtime .bonus-part  span').innerHTML = res.forty_nine[0].numbers.buster_lunchtime
+	document.querySelector('.s49-part-1 .s49-teatime .bonus-part span').innerHTML = res.forty_nine[0].numbers.buster_teatime
 
-	document.querySelector('.s49-part-2 .s49-lunchtime .bonus-part span').innerHTML = res.forty_nine[1].numbers[0].buster
-	document.querySelector('.s49-part-2 .s49-teatime .bonus-part span').innerHTML = res.forty_nine[1].numbers[1].buster
+	document.querySelector('.s49-part-2 .s49-lunchtime .bonus-part span').innerHTML = res.forty_nine[1].numbers.buster_lunchtime
+	document.querySelector('.s49-part-2 .s49-teatime .bonus-part span').innerHTML = res.forty_nine[1].numbers.buster_teatime
 
-	document.querySelector('.s49-part-3 .s49-lunchtime .bonus-part span').innerHTML = res.forty_nine[2].numbers[0].buster
-	document.querySelector('.s49-part-3 .s49-teatime .bonus-part span').innerHTML = res.forty_nine[2].numbers[1].buster
+	document.querySelector('.s49-part-3 .s49-lunchtime .bonus-part span').innerHTML = res.forty_nine[2].numbers.buster_lunchtime
+	document.querySelector('.s49-part-3 .s49-teatime .bonus-part span').innerHTML = res.forty_nine[2].numbers.buster_teatime
 
-	document.querySelector('.s49-part-4 .s49-lunchtime .bonus-part span').innerHTML = res.forty_nine[3].numbers[0].buster
-	document.querySelector('.s49-part-4 .s49-teatime .bonus-part span').innerHTML = res.forty_nine[3].numbers[1].buster
+	document.querySelector('.s49-part-4 .s49-lunchtime .bonus-part span').innerHTML = res.forty_nine[3].numbers.buster_lunchtime
+	document.querySelector('.s49-part-4 .s49-teatime .bonus-part span').innerHTML = res.forty_nine[3].numbers.buster_teatime
 
 
 	// ================================== DAILY LUCKY
 	for (let i = 0; i < document.querySelector('.daily-part-1 .daily-main-2 .general-part').childElementCount; i++) {
-		document.querySelector('.daily-part-1 .daily-main-2 .general-part').children[i].innerHTML = res.daily_lucky[0].numbers[0].main_2_pm[i]
-		document.querySelector('.daily-part-1 .daily-plus-2 .general-part').children[i].innerHTML = res.daily_lucky[0].numbers[1].plus_2_pm[i]
-		document.querySelector('.daily-part-1 .daily-main-9 .general-part').children[i].innerHTML = res.daily_lucky[0].numbers[2].main_9_pm[i]
-		document.querySelector('.daily-part-1 .daily-plus-9 .general-part').children[i].innerHTML = res.daily_lucky[0].numbers[3].plus_9_pm[i]
+		document.querySelector('.daily-part-1 .daily-main-2 .general-part').children[i].innerHTML = res.daily_lucky[0].numbers.main_2_pm[i]
+		document.querySelector('.daily-part-1 .daily-plus-2 .general-part').children[i].innerHTML = res.daily_lucky[0].numbers.plus_2_pm[i]
+		document.querySelector('.daily-part-1 .daily-main-9 .general-part').children[i].innerHTML = res.daily_lucky[0].numbers.main_9_pm[i]
+		document.querySelector('.daily-part-1 .daily-plus-9 .general-part').children[i].innerHTML = res.daily_lucky[0].numbers.plus_9_pm[i]
 
-		document.querySelector('.daily-part-2 .daily-main-2 .general-part').children[i].innerHTML = res.daily_lucky[1].numbers[0].main_2_pm[i]
-		document.querySelector('.daily-part-2 .daily-plus-2 .general-part').children[i].innerHTML = res.daily_lucky[1].numbers[1].plus_2_pm[i]
-		document.querySelector('.daily-part-2 .daily-main-9 .general-part').children[i].innerHTML = res.daily_lucky[1].numbers[2].main_9_pm[i]
-		document.querySelector('.daily-part-2 .daily-plus-9 .general-part').children[i].innerHTML = res.daily_lucky[1].numbers[3].plus_9_pm[i]
+		document.querySelector('.daily-part-2 .daily-main-2 .general-part').children[i].innerHTML = res.daily_lucky[1].numbers.main_2_pm[i]
+		document.querySelector('.daily-part-2 .daily-plus-2 .general-part').children[i].innerHTML = res.daily_lucky[1].numbers.plus_2_pm[i]
+		document.querySelector('.daily-part-2 .daily-main-9 .general-part').children[i].innerHTML = res.daily_lucky[1].numbers.main_9_pm[i]
+		document.querySelector('.daily-part-2 .daily-plus-9 .general-part').children[i].innerHTML = res.daily_lucky[1].numbers.plus_9_pm[i]
 	}
 	// ==== DAILY BOUNUS BALLS 
-	document.querySelector('.daily-part-1 .daily-main-2 .bonus-part span').innerHTML = res.daily_lucky[0].numbers[0].bonusball
-	document.querySelector('.daily-part-1 .daily-plus-2 .bonus-part span').innerHTML = res.daily_lucky[0].numbers[1].bonusball
-	document.querySelector('.daily-part-1 .daily-main-9 .bonus-part span').innerHTML = res.daily_lucky[0].numbers[2].bonusball
-	document.querySelector('.daily-part-1 .daily-plus-9 .bonus-part span').innerHTML = res.daily_lucky[0].numbers[3].bonusball
+	document.querySelector('.daily-part-1 .daily-main-2 .bonus-part span').innerHTML = res.daily_lucky[0].numbers.bonusball_main_2_pm
+	document.querySelector('.daily-part-1 .daily-plus-2 .bonus-part span').innerHTML = res.daily_lucky[0].numbers.bonusball_plus_2_pm
+	document.querySelector('.daily-part-1 .daily-main-9 .bonus-part span').innerHTML = res.daily_lucky[0].numbers.bonusball_main_9_pm
+	document.querySelector('.daily-part-1 .daily-plus-9 .bonus-part span').innerHTML = res.daily_lucky[0].numbers.bonusball_plus_9_pm
 
-	document.querySelector('.daily-part-2 .daily-main-2 .bonus-part span').innerHTML = res.daily_lucky[1].numbers[0].bonusball
-	document.querySelector('.daily-part-2 .daily-plus-2 .bonus-part span').innerHTML = res.daily_lucky[1].numbers[1].bonusball
-	document.querySelector('.daily-part-2 .daily-main-9 .bonus-part span').innerHTML = res.daily_lucky[1].numbers[2].bonusball
-	document.querySelector('.daily-part-2 .daily-plus-9 .bonus-part span').innerHTML = res.daily_lucky[1].numbers[3].bonusball
+	document.querySelector('.daily-part-2 .daily-main-2 .bonus-part span').innerHTML = res.daily_lucky[1].numbers.bonusball_main_2_pm
+	document.querySelector('.daily-part-2 .daily-plus-2 .bonus-part span').innerHTML = res.daily_lucky[1].numbers.bonusball_plus_2_pm
+	document.querySelector('.daily-part-2 .daily-main-9 .bonus-part span').innerHTML = res.daily_lucky[1].numbers.bonusball_main_9_pm
+	document.querySelector('.daily-part-2 .daily-plus-9 .bonus-part span').innerHTML = res.daily_lucky[1].numbers.bonusball_plus_9_pm
 
 	// ================================== UK LUCKY
 	for (let i = 0; i < document.querySelector('.uk-part-1 .uk-main .general-part').childElementCount; i++) {
-		document.querySelector('.uk-part-1 .uk-main .general-part').children[i].innerHTML = res.uk_lucky[0].numbers[0].main[i]
-		document.querySelector('.uk-part-2 .uk-main .general-part').children[i].innerHTML = res.uk_lucky[1].numbers[0].main[i]
-		document.querySelector('.uk-part-3 .uk-main .general-part').children[i].innerHTML = res.uk_lucky[2].numbers[0].main[i]
-		document.querySelector('.uk-part-4 .uk-main .general-part').children[i].innerHTML = res.uk_lucky[3].numbers[0].main[i]
+		document.querySelector('.uk-part-1 .uk-main .general-part').children[i].innerHTML = res.uk_lucky[0].numbers.main[i]
+		document.querySelector('.uk-part-2 .uk-main .general-part').children[i].innerHTML = res.uk_lucky[1].numbers.main[i]
+		document.querySelector('.uk-part-3 .uk-main .general-part').children[i].innerHTML = res.uk_lucky[2].numbers.main[i]
+		document.querySelector('.uk-part-4 .uk-main .general-part').children[i].innerHTML = res.uk_lucky[3].numbers.main[i]
 	}
 	// ==== UK BOUNUS BALLS 
-	document.querySelector('.uk-part-1 .uk-main .bonus-part span').innerHTML = res.uk_lucky[0].numbers[0].bonusball
-	document.querySelector('.uk-part-2 .uk-main .bonus-part span').innerHTML = res.uk_lucky[1].numbers[0].bonusball
-	document.querySelector('.uk-part-3 .uk-main .bonus-part span').innerHTML = res.uk_lucky[2].numbers[0].bonusball
-	document.querySelector('.uk-part-4 .uk-main .bonus-part span').innerHTML = res.uk_lucky[3].numbers[0].bonusball
+	document.querySelector('.uk-part-1 .uk-main .bonus-part span').innerHTML = res.uk_lucky[0].numbers.bonusball_main
+	document.querySelector('.uk-part-2 .uk-main .bonus-part span').innerHTML = res.uk_lucky[1].numbers.bonusball_main
+	document.querySelector('.uk-part-3 .uk-main .bonus-part span').innerHTML = res.uk_lucky[2].numbers.bonusball_main
+	document.querySelector('.uk-part-4 .uk-main .bonus-part span').innerHTML = res.uk_lucky[3].numbers.bonusball_main
 }
